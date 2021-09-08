8103aa17971a91ecebdb72994e364406eff540e6
RKostochko <kostochko.tech@mail.ru>
2021-09-08 18:05:35 +0300

    feat: add transform array

---
+++ src/dist/transform-array.dev.js
@@ -0,0 +1,67 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports["default"] = transform;
+
+var _index = require("../extensions/index.js");
+
+/**
+ * Create transformed array based on the control sequences that original
+ * array contains
+ * 
+ * @param {Array} arr initial array
+ * @returns {Array} transformed array
+ * 
+ * @example
+ * 
+ * transform([1, 2, 3, '--double-next', 4, 5]) => [1, 2, 3, 4, 4, 5]
+ * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
+ * 
+ */
+function transform(arr) {
+  if (!Array.isArray(arr)) {
+    throw new Error("'arr' parameter must be an instance of the Array!");
+  }
+
+  if (arr === []) {
+    return arr;
+  }
+
+  var newArr = [];
+
+  for (var i = 0; i < arr.length; i++) {
+    switch (arr[i]) {
+      case '--discard-next':
+        i += 1;
+        break;
+
+      case '--discard-prev':
+        if (arr[i - 2] !== '--discard-next') {
+          newArr.pop();
+        }
+
+        break;
+
+      case '--double-next':
+        if (i + 1 < arr.length) {
+          newArr.push(arr[i + 1]);
+        }
+
+        break;
+
+      case '--double-prev':
+        if (i - 1 >= 0 && arr[i - 2] !== '--discard-next') {
+          newArr.push(arr[i - 1]);
+        }
+
+        break;
+
+      default:
+        newArr.push(arr[i]);
+    }
+  }
+
+  return newArr;
+}
--- src/transform-array.js
+++ src/transform-array.js
@@ -13,7 +13,37 @@ import { NotImplementedError } from '../extensions/index.js';
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-export default function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function transform(arr) {
+  if (!Array.isArray(arr)) {
+	throw new Error("'arr' parameter must be an instance of the Array!");
+  } 
+  if (arr === []){
+	  return arr;
+  }
+  let newArr = [];
+  for (let i = 0; i < arr.length; i++) {
+	  switch(arr[i]){
+		  case ('--discard-next'):
+			  i += 1;
+			  break;
+			case ('--discard-prev'):
+				if(arr[i - 2] !== '--discard-next'){
+					newArr.pop()
+				}
+				break;
+			case ('--double-next'):
+				if(i + 1 < arr.length){
+					newArr.push(arr[i + 1]);
+				}
+				break;
+			case ('--double-prev'):
+				if(i - 1 >= 0 && arr[i - 2] !== '--discard-next'){
+					newArr.push(arr[i - 1])
+				}
+				break;
+			default:
+				newArr.push(arr[i]);
+	  }
+  }
+  return newArr;
 }
