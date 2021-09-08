f72b5c5d89b169d3d22dd4b83b274b40723bc4a7
RKostochko <kostochko.tech@mail.ru>
2021-09-08 15:41:02 +0300

    feat: add dream-team

---
+++ src/dist/dream-team.dev.js
@@ -0,0 +1,35 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports["default"] = createDreamTeam;
+
+var _index = require("../extensions/index.js");
+
+/**
+ * Create name of dream team based on the names of its members
+ *  
+ * @param {Array} members names of the members 
+ * @return {String | Boolean} name of the team or false
+ * in case of incorrect members
+ *
+ * @example
+ * 
+ * createDreamTeam(['Matt', 'Ann', 'Dmitry', 'Max']) => 'ADMM'
+ * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
+ *
+ */
+function createDreamTeam(members) {
+  if (!Array.isArray(members)) {
+    return false;
+  }
+
+  return members.filter(function (name) {
+    return typeof name === 'string';
+  }).map(function (name) {
+    return name.trim();
+  }).map(function (name) {
+    return name[0].toUpperCase();
+  }).sort().join('');
+}
